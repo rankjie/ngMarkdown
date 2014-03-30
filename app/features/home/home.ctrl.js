f2e3860ea7437f12cb36ac2267dc20996cf2b117
Gregory Pratt <gregory.pratt@me.com>
2014-03-30 14:16:12 +0100

    Updated controller

--- app/features/home/home.ctrl.js
+++ app/features/home/home.ctrl.js
@@ -1,10 +1,10 @@
 angular.module('demo.features.home')
 
-.controller('HomeCtrl', ['$scope', 'wizMarkdownSvc', function ($scope, wizMarkdownSvc) {
+.controller('HomeCtrl', ['$scope', '$http', 'wizMarkdownSvc', function ($scope, $http, wizMarkdownSvc) {
 
-	$scope.mdText = 'Loading text...';
+	$scope.mdtext = 'Loading text...';
 
 	$http.get('http://grumpywizards.com/ngMarkdown/app/features/home/home.md').then(function (data) {
-		$scope.mdText = data;
+		$scope.mdtext = data;
 	});
 }]);
