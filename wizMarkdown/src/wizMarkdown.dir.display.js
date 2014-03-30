a7eebf01c7fc61a2fea52d8a96bfbc391e2e6a04
Gregory Pratt <gregory.pratt@me.com>
2014-03-30 22:30:34 +0100

    Code highlighting added using highlight.js

---
+++ wizMarkdown/src/wizMarkdown.dir.display.js
@@ -0,0 +1,21 @@
+﻿angular.module('wiz.markdown')
+
+.directive('wizMarkdown', ['$filter', 'wizMarkdownSvc', function ($filter, wizMarkdownSvc) {
+    return {
+        restrict: 'E',
+        scope: {
+            'content': '='
+        },
+        replace: true,
+        template: '<div class="markdown-output"></div>',
+        link: function (scope, elem, attrs) {
+            scope.$watch('content', function () {
+                elem.html(wizMarkdownSvc.Transform(scope.content));
+                // Apply highlighting when required
+                angular.forEach(elem.find('pre'), function(value){
+                    hljs.highlightBlock(value);
+                });
+            });
+        }
+    };
+}]);
