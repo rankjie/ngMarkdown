642476977e8777ae3eb37d4f0ea6fbb623c10aa8
Gregory Pratt <gregory.pratt@me.com>
2014-03-30 14:56:31 +0100

    Couple of tweaks to loading behaviour

--- app/features/home/home.ctrl.js
+++ app/features/home/home.ctrl.js
@@ -3,8 +3,10 @@ angular.module('demo.features.home')
 .controller('HomeCtrl', ['$scope', '$http', 'wizMarkdownSvc', function ($scope, $http, wizMarkdownSvc) {
 
 	$scope.mdtext = 'Loading text...';
+	$scope.loaded = false;
 
 	$http.get('http://grumpywizards.com/ngMarkdown/app/features/home/home.md').then(function (response) {
 		$scope.mdtext = response.data;
+		$scope.loaded = true;
 	});
 }]);
