c53b0bfd4606ce5ac534911da8e7e2aae2864a2d
Greg <gregory.pratt@me.com>
2014-03-31 15:13:52 +0100

    Fixing indents

--- wizMarkdown/src/wizMarkdown.dir.editor.js
+++ wizMarkdown/src/wizMarkdown.dir.editor.js
@@ -9,10 +9,10 @@
 		replace: true,
 		transclude: true,
 		template: '<div class="markdown-editor">' +
-								'<div class="markdown-toolbar" ng-if="!toolbarBottom" ng-transclude></div>' +
-								'<textarea class="markdown-input" ng-model="content"></textarea>' +
-								'<div class="markdown-toolbar" ng-if="toolbarBottom" ng-transclude></div>' +
-							'</div>',
+		            '<div class="markdown-toolbar" ng-if="!toolbarBottom" ng-transclude></div>' +
+		            '<textarea class="markdown-input" ng-model="content"></textarea>' +
+		            '<div class="markdown-toolbar" ng-if="toolbarBottom" ng-transclude></div>' +
+		          '</div>',
 		controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) { }],
 		link: function (scope, elem, attrs, ctrl) {
 			var editor = new MarkdownDeepEditor.Editor(elem.find('textarea')[0], null);
