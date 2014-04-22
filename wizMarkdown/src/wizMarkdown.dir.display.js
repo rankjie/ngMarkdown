a2b78933ed3b285e9cbd0b1f871923390753f80d
Jeremy <rankjie@gmail.com>
2014-04-22 18:54:40 +0800

    Avoid potential error
    An error would raise if wizMarkdownSvc.Transform if content is undefined or null.

--- wizMarkdown/src/wizMarkdown.dir.display.js
+++ wizMarkdown/src/wizMarkdown.dir.display.js
@@ -10,6 +10,7 @@
         template: '<div class="markdown-output"></div>',
         link: function (scope, elem, attrs) {
             scope.$watch('content', function () {
+                if (!scope.content) return null;
                 elem.html(wizMarkdownSvc.Transform(scope.content));
                 // Apply highlighting when required
                 angular.forEach(elem.find('pre'), function(value){
